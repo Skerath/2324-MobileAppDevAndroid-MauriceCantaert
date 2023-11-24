f6e433df797aab3fee94a13eb172bbefcc14c9a1
Maurice Cantaert <maurice.cantaert@student.hogent.be>
2023-11-24 19:06:50 +0100

    Initial commit

---
+++ app/src/main/java/be/mauricecantaert/mobileappdevandroid/MainActivity.kt
@@ -0,0 +1,46 @@
+package be.mauricecantaert.mobileappdevandroid
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import be.mauricecantaert.mobileappdevandroid.ui.theme.MobileAppDevAndroidTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MobileAppDevAndroidTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background,
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier,
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MobileAppDevAndroidTheme {
+        Greeting("Android")
+    }
+}
