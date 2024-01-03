ab6133717016644ba1e9c717848c671683eea7f5
Maurice Cantaert <maurice.cantaert@student.hogent.be>
2024-01-03 18:32:28 +0100

    update dependencies

--- app/src/main/java/be/mauricecantaert/mobileappdevandroid/Application.kt
+++ app/src/main/java/be/mauricecantaert/mobileappdevandroid/Application.kt
@@ -1,10 +1,17 @@
 package be.mauricecantaert.mobileappdevandroid
 
+import android.content.Context
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
 
 @Composable
-fun Application() {
+fun Application(
+    navController: NavHostController = rememberNavController(),
+    context: Context = LocalContext.current,
+) {
     Text(
         text = "Test",
     )
